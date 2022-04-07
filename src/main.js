98d2ddff5f4fa416673fce46a24df06f5de69b90
Ruslan Hryshyn <ruslannightcore@outlook.com>
2022-04-07 14:47:36 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import SweetModal from './components/SweetModal'
-import SweetModalTab from './components/SweetModalTab'
+import SweetModal from './components/SweetModal.vue'
+import SweetModalTab from './components/SweetModalTab.vue'
 
 export {
 	SweetModal,
